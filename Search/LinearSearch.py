3ed15c0c2601503c558fbd18cd7f43314b765bbe
Aman <attar.aman29@gmail.com>
2022-12-26 18:08:05 -0600

    Linear Search Algorithm is Implemented

--- Search/LinearSearch.py
+++ Search/LinearSearch.py
@@ -1,1 +1,18 @@
 #code for linear search
+
+def linearSearch(arr, key):
+    for item in arr:
+        if item == key:
+            return True
+    return False
+
+if __name__ == "__main__":
+    array = []
+    no = int(input("Enter no of elements in the array :"))
+    for i in range(no):
+        array.append(int(input("Enter element :")))
+    key = int(input("Enter the key to be searched :"))
+    if linearSearch(array, key):
+        print("Number {} found in array".format(key))
+    else:
+        print("Number {} not found in array".format(key))
